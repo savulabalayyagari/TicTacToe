2bd72508855e2e95950c7c123f909ba516e1ad01
savulabalayyagari <savulabalayyagari@uiowa.edu>
2025-09-16 13:26:44 -0500

    Initial project setup: add all files

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import './App.css';
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </>
+  );
+}
+
